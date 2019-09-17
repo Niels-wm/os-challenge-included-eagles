490ae4214f88d7a5a5a367fe273f8ae81edade2c
Niels Mikkelsen <s174290@student.dtu.dk>
2019-09-17 09:37:27 +0200

    Added a bzero() to clear the packet1 struct before reciving the client request

--- server.c
+++ server.c
@@ -70,7 +70,7 @@ int main(int argc, char *argv[]) {
 
 
     /* Recive */
-    
+    bzero((char *)&packet1, sizeof(packet1));
     n = read(newSockFileDescripter, &packet1, sizeof(packet1));
 
     if (n < 0) {
