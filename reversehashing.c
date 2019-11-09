cd932b04f675133927de41c8f211eb168687417d
Helena Schiøtz <s174279@student.dtu.dk>
2019-11-09 13:19:02 +0100

    Update reversehashing.c

--- reversehashing.c
+++ reversehashing.c
@@ -70,7 +70,7 @@ void *reversehashing(void *arg) {
 		while (!(memcmp(theHash, packet.hash, sizeof(theHash)) == 0)) {
 			bzero(theHash, 32);
 			answer = (rand() % (sizeof(theHash) - start + 1)) + start;
-			printf("\nFound a match, with:  %" PRIu64, answer);
+			printf("\nRandomly Generated:  %" PRIu64, answer);
 			unsigned char *hashedNumber = SHA256((char*) &answer, 8, theHash);
 		}
       /*for (answer; answer <= packet.end; answer++){
