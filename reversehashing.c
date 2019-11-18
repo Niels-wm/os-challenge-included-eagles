0f28b9c38a607c48dbf1ce180cd18fa7083b437a
felix <s174296@student.dtu.dk>
2019-11-18 12:10:17 +0100

    Done

--- reversehashing.c
+++ reversehashing.c
@@ -10,28 +10,28 @@
 
 void reversehashing(void *tInfo, void *p) {
 
-    printf("started reversing\n");
+    //printf("started reversing\n");
     struct ThreadInfo* ti = (struct ThreadInfo*) tInfo;
     struct Packet* packetP = (struct Packet*) p;
 
     struct Packet packet = *packetP;
-    printf("packet %x \n", packet.start);
+    //printf("packet %x \n", packet.start);
     
 
-    printf("TI: %d", ti->fs);
+    //printf("TI: %d", ti->fs);
     uint8_t testHash[32];
     pthread_mutex_t* lock = (ti->lock);
     int fs = ti->fs;
     int i, n;
 
-    printf("part1 done\n");
+    //printf("part1 done\n");
 
     /* Receive */
     //bzero((char *)&packet, sizeof(packet));
-    //printf("part2\n");
+    ////printf("part2\n");
     //n = read(ti->fs, &packet, sizeof(packet));
 
-    //printf("part3\n");
+    ////printf("part3\n");
     //if (n < 0)
     //{
     //    perror("ERROR reading from socket");
@@ -43,25 +43,25 @@ void reversehashing(void *tInfo, void *p) {
     packet.end = be64toh(packet.end);
 
 
-    printf("hash: %x\n", packet.hash);
+    //printf("hash: %x\n", packet.hash);
     // /* SHA 256 ALGO */
     uint64_t answer = packet.start;
-    printf("packet start: %d\n", packet.start);
+    //printf("packet start: %d\n", packet.start);
     uint8_t theHash[32];
-    printf("prelock\n");
+    //printf("prelock\n");
     // -- CHECK IF RECEIVED HASH IS A KNOWN HASH (IN HASHTABLE) AND SEND ANSWER TO CLIENT IF IT IS:
     pthread_mutex_lock(lock);
     uint64_t foundAnswer = find(packet.hash);
     pthread_mutex_unlock(lock);
-    printf("postlock\n");
-    // printf("\nFOUND value:  %" PRIu64, foundAnswer);
+    //printf("postlock\n");
+    // //printf("\nFOUND value:  %" PRIu64, foundAnswer);
 
     bzero(testHash, 32);
     SHA256((char*) &foundAnswer, 8, testHash);
 
 
     if (foundAnswer != 0 && memcmp(testHash, packet.hash, sizeof(testHash)) == 0){
-      printf("\nFOUND in hashtable\n");
+      //printf("\nFOUND in hashtable\n");
       foundAnswer = be64toh(foundAnswer);
       n = write(fs, &foundAnswer, 8);
 
@@ -77,7 +77,7 @@ void reversehashing(void *tInfo, void *p) {
         unsigned char *hashedNumber = SHA256((char*) &answer, 8, theHash);
 
         if (memcmp(theHash, packet.hash, sizeof(theHash)) == 0) {
-          //printf("\nFound a match, with:  %" PRIu64, answer);
+          ////printf("\nFound a match, with:  %" PRIu64, answer);
           break;
         }
       }
--- server.c
+++ server.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <poll.h>
 #include "messages.h"
 #include "packet.h"
 #include "reversehashing.h"
@@ -22,6 +25,7 @@
 int threadAmount;
 pthread_mutex_t* lock; 
 pthread_mutex_t* queLock;
+pthread_mutex_t* threadsLock;
 struct Packet packets[100];
 struct Node* head = NULL;
 int threadActive[3];
@@ -29,40 +33,63 @@ pthread_t new;
 
 
 struct ThreadInfo* getNextTi(struct Packet *p){
-    struct ThreadInfo* returnVal = head->ti;
-    memcpy(p,&head->packet,sizeof(struct Packet));
-    struct Node* oldhead = head;
-    head = head->next;
-    free(oldhead);
+    struct ThreadInfo* returnVal = NULL;
+    if(head != NULL){
+        returnVal = head->ti;
+        memcpy(p,&head->packet,sizeof(struct Packet));
+        struct Node* oldhead = head;
+        head = head->next;
+        free(oldhead);
+    }
+    else{
+        //printf("head is null, gonna seg fault");
+    }
+
     return returnVal;
 }
 
 void *startReverseHashing(void *arg){
     struct Packet* packet = malloc(sizeof(struct Packet));
+    //printf("locking quelock\n");
     pthread_mutex_lock(queLock);
+    //printf("getting nedxt ti\n");
     struct ThreadInfo* ti = getNextTi(packet);
+    //printf("Got ti\n");
     pthread_mutex_unlock(queLock);
-    threadAmount++;
-    reversehashing(ti,packet);
+     //printf("unlocked quelock\n");
+    if(ti != NULL){
+        
+
+        reversehashing(ti,packet);
+    
+    }
+    pthread_mutex_lock(threadsLock);
     threadAmount--;
-    printf("Done\n");
-    pthread_exit(NULL);
+    //printf("nr of threads: %d", threadAmount);
+    pthread_mutex_unlock(threadsLock);
+    //printf("Done\n");
 }
 
 
 int main(int argc, char *argv[]) {
-printf("innnnnnnnit\n");
+//printf("innnnnnnnit\n");
     int sockFileDescripter, newSockFileDescripter;
     struct sockaddr_in serverAddr, clientAddr;
     socklen_t clientAddrSize;
     int n, i, pid, err;
+    int wait = 0;
+    struct pollfd fds[1];
+    int timeout_msecs = 3000;
+    int ret;
+    threadAmount = 0;
     
     lock = malloc(sizeof(pthread_mutex_t));
     queLock = malloc(sizeof(pthread_mutex_t));
+    threadsLock = malloc(sizeof(pthread_mutex_t));
     pthread_mutex_init(lock, NULL);
     pthread_mutex_init(queLock, NULL);
     
-    printf("innit\n");
+    //printf("innit\n");
     //pthread_t thread;
 
 
@@ -71,7 +98,7 @@ printf("innnnnnnnit\n");
         perror("ERROR opening socket");
         exit(1);
     }
-    printf("1");
+    //printf("1");
     /* Disable safety feature */
     // The operating system sets a timeout on TCP sockets after using them.
     // It does that to make sure that all information from the old program
@@ -79,7 +106,7 @@ printf("innnnnnnnit\n");
     int option = 1;
     setsockopt(sockFileDescripter, SOL_SOCKET, SO_REUSEADDR, &option, sizeof(option));
 
-printf("2");
+//printf("2");
     /* Initialize socket structure */
     // bzero() is used to set all the socket structures with null values. It does the same thing as the following:
     // memset(&serverAddr, '\0', sizeof(serverAddr));
@@ -89,73 +116,135 @@ printf("2");
     serverAddr.sin_port = htons(PORT);
     serverAddr.sin_addr.s_addr = INADDR_ANY;
 
-printf("3");
+//printf("3");
     /* Bind */
     if (bind(sockFileDescripter, (struct sockaddr *) &serverAddr, sizeof(serverAddr)) < 0){
         perror("ERROR on binding");
         exit(1);
     }
-printf("4");
+//printf("4");
     /* Listen */
     listen(sockFileDescripter, 50);
     clientAddrSize = sizeof(clientAddr);
 
     // // Put the accept statement and the following code in an infinite loop /*
-    printf("hi");
-    i = 2;
+    //printf("hi");
+    fcntl(sockFileDescripter, F_SETFL, O_NONBLOCK);
+
+    fds[0].fd = sockFileDescripter;
+    fds[0].events = POLLIN;
     while (1) {
-        // printf("\nHere are the i: %d", i);
+        // //printf("\nHere are the i: %d", i);
         /* Accept */
-        printf("accepting...");
-        newSockFileDescripter = accept(sockFileDescripter, (struct sockaddr *)&clientAddr, &clientAddrSize);
-        printf("accepted!");        
-        if (newSockFileDescripter < 0){
-            perror("ERROR on accept");
-            exit(1);
-        }
-        struct ThreadInfo* ti = malloc(sizeof(struct ThreadInfo));
-        ti->fs = newSockFileDescripter;
-        ti->lock = lock;
-
-        struct Node* request = malloc(sizeof(struct Node));
-        struct Packet packet;
-
-        
-        
-        n = read(ti->fs, &packet, sizeof(packet));
-
-        if (n < 0)
-        {
-            perror("ERROR reading from socket");
-            exit(1);
-        }
-        request->packet = packet;
-        request->ti = ti;
-        
-        struct Node* node = head;
-        pthread_mutex_lock(queLock);
-        if(node != NULL){
-            while(node != NULL){
-                if(node->next != NULL){
-                    if(request->packet.p <= node->packet.p && request->packet.p > node->next->packet.p){
-                    request->next = node->next;
-                    node->next = request;
-                    }
-                }
-                else{
-                    request->next = NULL;
-                    node->next = request;
-                }
-                
+        //printf("polling...\n");
+        ret = poll(fds, 2, timeout_msecs);
+        //printf("poll done\n");
+        if(ret > 0 && (fds[0].revents & POLLIN)){
+            //printf("accepting\n");
+            newSockFileDescripter = accept(sockFileDescripter, (struct sockaddr *)&clientAddr, &clientAddrSize);
+            //printf("accepted!\n");        
+            if (newSockFileDescripter < 0 ){
+                perror("ERROR on accept");
+                exit(1);
             }
+    
+            struct ThreadInfo* ti = malloc(sizeof(struct ThreadInfo));
+            ti->fs = newSockFileDescripter;
+            ti->lock = lock;
+
+            struct Node* request = malloc(sizeof(struct Node));
+            struct Packet packet;
+
+            //printf("print1");
+            
+            n = read(ti->fs, &packet, sizeof(packet));
+
+            if (n < 0)
+            {
+                perror("ERROR reading from socket");
+                exit(1);
+            }
+            request->packet = packet;
+            request->ti = ti;
+            
+            struct Node* node = head;
+            //printf("print2");
+            //printf("locked quelock\n");
+            pthread_mutex_lock(queLock);
+            if(node != NULL){
+                //printf("node not null\n");
+            }
+            if(request != NULL){
+                //printf("request not null\n");
+            }
+            if(node != NULL){
+                while(node != NULL){
+                    //printf("request p: %d, node p: %d, next p: xxxx.\n",request->packet.p,node->packet.p/*,node->next->packet.p*/);
+                    if(node->next != NULL){
+                        //printf("next p: %d\n", node->next->packet.p);
+                        if(request->packet.p <= node->packet.p && request->packet.p >= node->next->packet.p){
+
+                            //printf("hi");
+                            request->next = node->next;
+                            node->next = request;
+                            break;
+                        }
+                        else if (request->packet.p >= node->packet.p){
+                            request->next = node;
+                            head = request;
+                            break;
+                        }
+                        else{
+                            node = node->next;
+                        }
+                    }
+                    else{
+                        //printf("entered else\n");
+                        request->next = NULL;
+                        node->next = request;
+                        break;
+                    }
+                    
+                }
+            }
+            else{
+                head = request;
+            }
+            
+            pthread_mutex_unlock(queLock);
+            //printf("unlocked quelock\n");
+            
         }
-        else{
-            head = request;
-        }
-        pthread_mutex_unlock(queLock);
         
-        if(threadAmount < 1){
+        pthread_mutex_lock(threadsLock);
+        if(threadAmount < 3){
+
             err = pthread_create(&new, NULL, startReverseHashing, NULL);
+               
+            threadAmount++;
+            //printf("nr of threads: %d", threadAmount);
+
         }
+        else if(threadAmount = 3){
+            wait = 1;
+            //printf("waiting...\n");
+        }
+        pthread_mutex_unlock(threadsLock);
+        
+        //printf("locking quelock main\n");
+        //printf("nr of threads: %d", threadAmount);
+        pthread_mutex_lock(threadsLock);
+        if(threadAmount < 3){
+            wait = 0;
+            //printf("resuming");
+            err = pthread_create(&new, NULL, startReverseHashing, NULL);
+            threadAmount++;
+            //printf("nr of threads: %d", threadAmount);
+        }
+        pthread_mutex_unlock(threadsLock);
+        //printf("unlocked quelock\n");
+
+        
+
     }
 }
