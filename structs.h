7702ba9e12647fa755a645b3d7435e3e1318b454
Niels Mikkelsen <31999893+Niels-wm@users.noreply.github.com>
2019-11-18 02:28:09 +0100

    Merging threadpool thread into master

--- structs.h
+++ structs.h
@@ -1,14 +1,19 @@
+
+#ifndef STRUCTS_H
+#define STRUCTS_H
+
 #include <inttypes.h>
 
 struct Packet {
-    uint8_t hash[32];
-    uint64_t start;
-    uint64_t end;
-    uint8_t p;
+   uint8_t hash[32];
+   uint64_t start;
+   uint64_t end;
+   uint8_t p;
 };
 
 struct Request {
     struct Packet packet;
     uint64_t prio;
     int reply_socket;
-};
+
+#endif
